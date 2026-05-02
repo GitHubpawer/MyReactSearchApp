fbaf65f73e0f975bd424c0901dc87d02f56aa97e
杉山建太朗 <ken19990914@icloud.com>
2026-05-02 21:00:01 +0900

    TitleBox追加

---
+++ src/components/Form.tsx
@@ -0,0 +1,10 @@
+const Form = () => {
+    return (
+        <form>
+            <input type="text" name="keyword" placeholder="Search for an image" />
+            <button type="submit">Search</button>
+        </form>
+    )
+};
+
+export default Form;
---
+++ src/components/TitleBox.tsx
@@ -0,0 +1,9 @@
+const TitleBox = () => {
+    return(
+        <>
+            <h1>Image Search App</h1>
+        </>
+    )
+}
+
+export default TitleBox;
